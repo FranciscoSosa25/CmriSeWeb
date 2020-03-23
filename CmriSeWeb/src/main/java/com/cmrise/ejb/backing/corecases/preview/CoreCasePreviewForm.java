d7a2aeebaf55951539e31d08482a8a24e04a024d
Gabino <gabino.hernandez@dcprosoft.com.mx>
2020-03-23 14:33:45 -0600

    V2

---
+++ CmriSeWeb/src/main/java/com/cmrise/ejb/backing/corecases/preview/CoreCasePreviewForm.java
@@ -0,0 +1,10 @@
+package com.cmrise.ejb.backing.corecases.preview;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CoreCasePreviewForm {
+
+}
